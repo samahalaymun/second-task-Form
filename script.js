06037f69f705b01a1351b58c408e506686a945c5
samah <samah_abu_laymun@hotmail.com>
2022-08-08 13:06:03 +0300

    Add some changes on inputs properties in index.html file ,reformat code in all files.

--- script.js
+++ script.js
@@ -1,85 +1,84 @@
 //const fs = require("fs");
-let dropdown=document.getElementById('country');
+let dropdown = document.getElementById('country');
 dropdown.length = 0;
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Select your country';
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
 let option;
-let data=[{
-    "country":"Algeria",
-    "value":"Algeria"
-},{
-    "country":"Egypt",
-    "value":"Egypt"
+let data = [{
+    "country": "Algeria",
+    "value": "Algeria"
+}, {
+    "country": "Egypt",
+    "value": "Egypt"
 },
 {
-    "country":"Iran",
-    "value":"Iran"
+    "country": "Iran",
+    "value": "Iran"
 },
 {
-    "country":"Iraq",
-    "value":"Iraq"
+    "country": "Iraq",
+    "value": "Iraq"
 },
 {
-    "country":"Israel",
-    "value":"Israel"
+    "country": "Israel",
+    "value": "Israel"
 },
 {
-    "country":"Jordan",
-    "value":"Jordan"
+    "country": "Jordan",
+    "value": "Jordan"
 },
 {
-    "country":"Kuwait",
-    "value":"Kuwait"
+    "country": "Kuwait",
+    "value": "Kuwait"
 },
 {
-    "country":"Lebanon",
-    "value":"Lebanon"
+    "country": "Lebanon",
+    "value": "Lebanon"
 },
 {
-    "country":"Libya",
-    "value":"Libya"
+    "country": "Libya",
+    "value": "Libya"
 },
 {
-    "country":"Morocco",
-    "value":"Morocco"
+    "country": "Morocco",
+    "value": "Morocco"
 },
 {
-    "country":"Oman",
-    "value":"Oman"
+    "country": "Oman",
+    "value": "Oman"
 },
 {
-    "country":"Qatar",
-    "value":"Qatar"
+    "country": "Qatar",
+    "value": "Qatar"
 },
 {
-    "country":"Saudi Arabia",
-    "value":"Saudi Arabia"
+    "country": "Saudi Arabia",
+    "value": "Saudi Arabia"
 },
 {
-    "country":"Syria",
-    "value":"Syria"
+    "country": "Syria",
+    "value": "Syria"
 },
 {
-    "country":"Tunisia",
-    "value":"Tunisia"
+    "country": "Tunisia",
+    "value": "Tunisia"
 },
 {
-    "country":"United Arab Emirates",
-    "value":"United Arab Emirates"
+    "country": "United Arab Emirates",
+    "value": "United Arab Emirates"
 },
 {
-    "country":"Yemen",
-    "value":"Yemen"
+    "country": "Yemen",
+    "value": "Yemen"
 },
 ]
-  for (let i = 0; i < data.length; i++) {
+for (let i = 0; i < data.length; i++) {
     option = document.createElement('option');
-      option.text = data[i].country;
-      option.value = data[i].value;
-      dropdown.add(option);
-  }
+    option.text = data[i].country;
+    option.value = data[i].value;
+    dropdown.add(option);
+}
 
 
-  
